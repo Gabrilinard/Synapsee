9af1026f23984d215c814b793f256689fa007906
jpdsafg <jpdsag@gmail.com>
2024-06-11 21:12:28 -0300

    Teste

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,10 @@
 import { React, Fragment } from "react"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
+import Home from "./pages/Home/index.jsx"
 import Login from "./pages/Login"
 import Registro from "./pages/Registro"
 import Usuario from "./pages/Usuario" 
+import Buscar from "./pages/Buscar" 
 import useAuth from "./hooks/useAuth.jsx";
 
 
@@ -20,6 +21,7 @@ export default function AppRoutes() {
             <Fragment>
                 <Routes>
                     <Route exact path="/" element={<Private Item={Home} />} />
+                    <Route exact path="/Buscar" element={<Private Item={Buscar} />} />
                     <Route exact path="/Usuario" element={<Private Item={Usuario} />} />
                     <Route path="/Login" element={<Login />} />
                     <Route exact path="/Registro" element={<Registro />} />
